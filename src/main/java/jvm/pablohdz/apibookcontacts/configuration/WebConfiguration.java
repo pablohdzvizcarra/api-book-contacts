c46ecaf7c386ebfc8e419496094b5978d36a01e3
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-11-22 22:31:32 -0600

    feat config swagger and cors

---
+++ src/main/java/jvm/pablohdz/apibookcontacts/configuration/WebConfiguration.java
@@ -0,0 +1,21 @@
+package jvm.pablohdz.apibookcontacts.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**")
+                .allowedOrigins("**")
+                .allowedMethods("**")
+                .maxAge(3600L)
+                .allowedHeaders("**")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
