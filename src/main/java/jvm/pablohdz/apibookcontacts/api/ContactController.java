63f3e20f0198223a6cd4d3f6964d71f0a3dac566
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-11-23 21:08:33 -0600

    feat catch id from the request

--- src/main/java/jvm/pablohdz/apibookcontacts/api/ContactController.java
+++ src/main/java/jvm/pablohdz/apibookcontacts/api/ContactController.java
@@ -2,9 +2,12 @@ package jvm.pablohdz.apibookcontacts.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,7 @@ import java.util.Map;
 
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 
 import jvm.pablohdz.apibookcontacts.model.ContactDto;
 import jvm.pablohdz.apibookcontacts.model.ContactRequest;
@@ -28,14 +32,13 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/contact")
 @RequiredArgsConstructor
-public class ContactController
-{
+@Validated
+public class ContactController {
     private final ContactService contactService;
 
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<DefaultResponse> save(@Valid @RequestBody ContactRequest request)
-    {
+    public ResponseEntity<DefaultResponse> save(@Valid @RequestBody ContactRequest request) {
         ContactDto data = contactService.create(request);
 
         return ResponseEntity.ok(DefaultResponse.builder()
@@ -50,8 +53,7 @@ public class ContactController
     }
 
     @GetMapping
-    public ResponseEntity<DefaultResponse> read()
-    {
+    public ResponseEntity<DefaultResponse> read() {
         Collection<ContactDto> dtos = contactService.read();
 
         return ResponseEntity.ok(DefaultResponse.builder()
@@ -65,12 +67,16 @@ public class ContactController
         );
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") @Min(1) Long id) {
+        return ResponseEntity.ok("it's works");
+    }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<DefaultResponse> handleMethodArgumentNotValid(
             MethodArgumentNotValidException exception
-    )
-    {
+    ) {
         HashMap<String, String> errors = new HashMap<>();
 
         exception.getBindingResult()
@@ -89,18 +95,25 @@ public class ContactController
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    ResponseEntity<Map<String, String>> handleConstrainsViolationException(
+    ResponseEntity<DefaultResponse> handleConstrainsViolationException(
             ConstraintViolationException exception
-    )
-    {
+    ) {
         Map<String, String> errors = new HashMap<>();
         exception.getConstraintViolations().forEach(constraintViolation ->
         {
             errors.put("message", constraintViolation.getMessage());
             errors.put("path", constraintViolation.getPropertyPath().toString());
         });
-        return ResponseEntity.ok(errors);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(DefaultResponse.builder()
+                .timeStamp(LocalDateTime.now().format(DefaultResponse.formatter))
+                .developerMessage("exception occurred when try validate path variables from the " +
+                        "request")
+                .data(Map.of("error", errors))
+                .message("validations path variables errors")
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .reason(HttpStatus.INTERNAL_SERVER_ERROR.toString())
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build());
     }
 
 }
